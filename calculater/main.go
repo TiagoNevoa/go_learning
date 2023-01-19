e6ee8f1ed0fea1fae192c652768cb922efddd171
Tiago Nevoa <tiagonevoa@gmail.com>
2023-01-19 12:51:46 +0000

    package list

---
+++ calculater/main.go
@@ -0,0 +1,16 @@
+package calculater
+
+import{
+	// Package bufio implements buffered Input/Output (Reader,Writer,Scanner).
+	"bufio"
+	// Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+	"fmt"
+	// Package math provides basic constants and mathematical functions.
+	"math"
+	// Package os provides a platform-independent interface to operating system functionality(error handling).
+	"os"
+	// Package strconv implements conversions to and from string representations of basic data types.
+	"strconv"
+	// Package strings implements simple functions to manipulate UTF-8 encoded strings.
+	"strings"
+}
