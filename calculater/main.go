34631200e92b04e93ce896cba174e6ac8190ea2a
Tiago Nevoa <tiagonevoa@gmail.com>
2023-01-25 14:39:19 +0000

    calculater v1.0

--- calculater/main.go
+++ calculater/main.go
@@ -1,45 +1,67 @@
 package main
 
 import (
-	// Package bufio implements buffered Input/Output (Reader,Writer,Scanner).
 	"bufio"
-	// Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
 	"fmt"
-	// Package math provides basic constants and mathematical functions.
-	"math"
-	// Package os provides a platform-independent interface to operating system functionality(error handling).
 	"os"
-	// Package strconv implements conversions to and from string representations of basic data types.
 	"strconv"
-	// Package strings implements simple functions to manipulate UTF-8 encoded strings.
 	"strings"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
 
-	// read line from terminal
-	reader := bufio.NewReader(os.Stdin)
-	// write on the terminal
-	fmt.Print("Value 1: ")
-	// save the line on var
-	input1, _ := reader.ReadString('\n')
-	// convert input String to float
-	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
-	// check if there is err
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 1 must be a number")
-	}
+	value1 := getInput("Value 1")
+	value2 := getInput("Value 2")
 
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
-	}
+	var result float64
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+	switch operation := getOperation(); operation {
+	case "+":
+		result = addValues(value1, value2)
+	case "-":
+		result = subtractValues(value1, value2)
+	case "*":
+		result = multiplyValues(value1, value2)
+	case "/":
+		result = divideValues(value1, value2)
+	default:
+		panic("Invalid operation")
+	}
+	fmt.Printf("The result is %v\n\n", result)
+
+}
+
+func getInput(prompt string) float64 {
+	fmt.Printf("%v: ", prompt)
+	input, _ := reader.ReadString('\n')
+	value, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+	if err != nil {
+		message := fmt.Sprintf("%v must be a number", prompt)
+		panic(message)
+	}
+	return value
+}
+
+func getOperation() string {
+	fmt.Print("Select an operation (+ - * /): ")
+	op, _ := reader.ReadString('\n')
+	return strings.TrimSpace(op)
+}
+
+func addValues(value1, value2 float64) float64 {
+	return value1 + value2
+}
+
+func subtractValues(value1, value2 float64) float64 {
+	return value1 - value2
+}
+
+func multiplyValues(value1, value2 float64) float64 {
+	return value1 * value2
+}
+
+func divideValues(value1, value2 float64) float64 {
+	return value1 / value2
 }
